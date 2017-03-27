d1e14cb417013effee4c8d6f366873d502fa7c44
Quinten Bidoul <quinten.bidoul@gmail.com>
2017-03-26 23:03:23 -0400

    Fixed the parallax containers.
    Signed-off-by: Quinten Bidoul <quinten.bidoul@gmail.com>

--- js/init.js
+++ js/init.js
@@ -1,19 +1,8 @@
-(function ($) {
-    $(function () {
+(function($){
+  $(function(){
 
-        $('.button-collapse').sideNav;
-        $('.parallax').parallax;
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
 
-    }); // end of document ready
-
-    $('.member').each(function () {
-        var $this = $(this);
-
-        if ('Offline' != $this.find('.viewers').text()) {
-            $this.find('.circle').addClass('online');
-        }
-        else {
-            $this.find('.circle').addClass('offline');
-        }
-    });
+  }); // end of document ready
 })(jQuery); // end of jQuery name space
